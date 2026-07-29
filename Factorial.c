a766769df42b6e9eed786f21e3be2ec4827a3c5a
sahana-ops <sahana45079@gmail.com>
2026-07-29 10:33:53 +0530

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,14 @@
+#include<stdip.h>
+void main()
+{
+  int i,fact=1,n;
+ptintf("Enter the number");
+scanf("%d",&n);
+do
+{
+fact=fact*i;
+i=i+1;
+}
+  while(i<=n);
+printf("the factorial is:%d",fact);
+}
